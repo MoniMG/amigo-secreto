5c35355da474d51cb5ab23628d5040da52b0b55c
MoniMG <montes.gaspar.monica@gmail.com>
2025-03-13 03:11:58 -0600

    Implementa una función para sortear los amigos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // Crear l
 let amigos = [];
 
+
 // Función para agregar  un nuevo amigo 
 function agregarAmigo (){
     let amigoAgregado = document.getElementById('amigo').value; // Lectura de elmento de entrada de textbox
@@ -11,16 +12,16 @@ function agregarAmigo (){
     if (amigoAgregado === "" ){
         alert ("Por favor, inserte un nombre");
         }
-    else {
-                        amigos.push(amigoAgregado); //Agrega el nombre al array
-                        console.log(amigos); //Visualisación de lista de amigos 
-                        document.querySelector('#amigo').value = ''; // Limpia el campo de entrada
-                        actualizarLista(); // Actualiza la lista de amigos HTML
-                                }
-   
+   else {
+        amigos.push(amigoAgregado); //Agrega el nombre al array
+        console.log(amigos); //Visualisación de lista de amigos 
+        document.querySelector('#amigo').value = ''; // Limpia el campo de entrada
+        actualizarLista(); // Actualiza la lista de amigos HTML
+    
+   }              
 }
-sortearAmigo;
-  
+
+
 //Implementa una función para actualizar la lista de amigos
 function actualizarLista(){
     let listaAmigos = document.getElementById('listaAmigos');
@@ -33,3 +34,23 @@ function actualizarLista(){
     }
 }
 
+
+// Implementa una función para sortear los amigos
+function sortearAmigo(){
+
+    //Validar que haya amigos disponibles:
+    if (amigos.length===0){
+        alert ("No hay amigos en la lista de Amigos, ingresa por lo menos 2 nombres");
+        return;
+    }
+  // Genera indice aleatorio 
+    let indice =  Math.floor(Math.random()*amigos.length);  
+        
+    // Obtener el nombre sorteado
+    let amigoSorteado = amigos[indice];
+
+    // Mostrar el resultado:
+    let resultadoNombre = document.getElementById('resultado');
+    resultadoNombre.innerHTML =  `El amigo secreto es: ${amigoSorteado}`;
+
+}
