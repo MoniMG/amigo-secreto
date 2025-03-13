acb5d46a07534494969a8ffa8b35cce692e4a4d6
MoniMG <montes.gaspar.monica@gmail.com>
2025-03-13 02:22:09 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Crear l
-let listaAmigos = [];
+let amigos = [];
 
 // Función para agregar  un nuevo amigo 
 function agregarAmigo (){
     let amigoAgregado = document.getElementById('amigo').value; // Lectura de elmento de entrada de textbox
-
     // Validación de nombre ingresado 
+
     if (amigoAgregado === "" ){
-        alert ('Por favor ingresa un nombre válido');
-        return;
+        alert ("Por favor, inserte un nombre");
         }
-
-         // Validación de lista de amigos
-    if (listaAmigos.includes(amigoAgregado) ){
-        alert ('Por favor ingresa otro nombre válido');
-        return;
-        }
-
-        // Amigo existente en la lista 
-        const regex = /^[a-zA-Z\s]+$/;
-        if (!regex.test(amigoAgregado)) {
-            alert("Nombre inválido. Solo se permiten letras.");
-            return;
-        }
-    listaAmigos.push(amigoAgregado);
-    console.log(listaAmigos); //Visualisación de lista de amigos 
+    else {
+                        amigos.push(amigoAgregado); //Agrega el nombre al array
+                        console.log(amigos); //Visualisación de lista de amigos 
+                        document.querySelector('#amigo').value = ''; // Limpia el campo de entrada
+                        actualizarLista(); // Actualiza la lista de amigos HTML
+                                }
+   
 }
+sortearAmigo;
   
+//Implementa una función para actualizar la lista de amigos
+function actualizarLista(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML= ""; // Limpia la lista
+
+    for(let i= 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
